280f9c645100d18d31084d0b287443fce7144821
Ferdinandfp <ferdinandfp@gmail.com>
2019-09-19 09:56:07 +1000

    Finished adding users and dry run
    Finished the logic for adding users to the database and implemented the dry_run (tesing script/functions) feature.

--- user_upload.php
+++ user_upload.php
@@ -2,10 +2,16 @@
 /* This script will read a CSV file containing a list of users with first, last name and
  * email address. The names will be formated to lower case and first letter uppercase.
  * The email address will be checked for the correct format and the records will be rejected if
- * the fromat is incorrect. The valid users will be inserted into a database with a 'users' table.
+ * the format is incorrect. The valid users will be inserted into a database into a 'users' table.
  * The table will be created or rebuild if required.
- * Directives table:
- * Note: using the --help opotins will precede all other options.
+ * Directives --create_table: The table with the name 'users' will be created, if the table exists
+ * an option will let you rebuild the table loosing all data or abort the table creation. If the --create_table
+ * is used together with the --file directive and the table creation/rebult is aborted no data will be inserted
+ * into the database.
+ * Directive --file: If the 'users' table does not exist and the --create_table directive is not used the user
+ * will be informend that the table does not exist and the option is provided to create it. If no, the table will
+ * not be created and no users are inserted into the database.
+ * Note: using the --help optoin will precede all other options.
  * Help/directives for user_upload.ph
  * --file [csv name] - the name of the csv file to be parsed
  * --create_table - this will cause the MuSQL users table to be build (and no further action will be taken)
@@ -16,17 +22,12 @@
  * -h - MySQL host name
  * -d - MySQL database name - optional, default is 'dbusers'
 */
-$directivesHelp =  "TBA";
-//Create constants for long options for multiple use.
+//Create constants for long options for possible, multiple use.
 define("OPTIONFILE", "file");
 define("OPTIONHELP", "help");
 define("OPTIONCREATETABLE", "create_table");
 define("OPTIONDRYRUN", "dry_run");
-//When processing the directives keep track if the option are used correclty.
-$invalidUsage = false;
-$issetHelp = false;
-$issetCreateTable = false;
-$issetDryRun = false;
+$issetDryRun = 0;
 $fileName = "";
 $dbUser = "";
 $dbPassword = "";
@@ -55,53 +56,52 @@ $options = getopt($shortoptions, $longoptions);
 foreach(array_keys($options) as $option) switch ($option){
     case 'u':
         $dbUser = $options['u'];
-        //echo $dbUser;
         break;
     case 'p':
         $dbPassword = $options['p'];
-        //echo $dbPassword;
         break;
     case 'h':
         $dbHost = $options['h'];
-        //echo $dbHost;
         break;
     case 'd':
         $dbName = $options['d'];
-        //echo $dbName;
         break;
     default:
-        //echo "Default switch";
+        //Default switch
 }
-//Catch the --help option, will ignore any other option and show the directives help
+//Catch the --help option, will ignore any other option and show the help information
 if(array_key_exists(OPTIONHELP, $options)){
     printHelp();
     exit(1);
 }
+//Catch the --dry run option and set the variable switch
+if(array_key_exists(OPTIONDRYRUN, $options)){
+    $issetDryRun = 1;
+    //Run script without adding data to DB";
+}
 //Catch the --create_table option, this will ignore any other options apart help and create the table
 if(array_key_exists(OPTIONCREATETABLE, $options)){
-    $issetCreateTable = true;
-    //echo "Create the DB table 'users' and nothing else";
     createUsersTable($dbName, $tableName, $dbUser, $dbPassword, $dbHost);
     if(!array_key_exists(OPTIONFILE, $options))exit(1);
 }
-//Catch the --dry run option and set the variable switch
-if(array_key_exists(OPTIONDRYRUN, $options)){
-    $issetDryRun = true;
-    //Run script without adding data to DB";
-}
-//Catch the --dry run option and set the variable switch
+//Catch the --file option and set the variable filename
 if(array_key_exists(OPTIONFILE, $options)){
     $fileName = $options[OPTIONFILE];
 }
+//if the database user details and file name are given
 if(validateUserDetails($dbUser, $dbPassword, $dbHost) && !empty($fileName)){
     $userResults = getData($fileName);
-    updateUsers($userResults, $dbName, $dbUser, $dbPassword, $dbHost, $tableName);
+    updateUsers($userResults, $dbName, $dbUser, $dbPassword, $dbHost, $tableName, $issetDryRun);
+}else{
+    echo "Please provide the correct user, host and/or input file information.";
 }
 //--Funtions------------------------------------------------------------------------
 //Check if a specific table exits and rebuilt the table if required
-function createUsersTable($dbname, $username, $password, $hostname){
+function createUsersTable($dbname, $tablename, $username, $password, $hostname){
     if(validateUserDetails($username, $password, $hostname)){
-        createTable($dbname, $username, $password, $hostname);
+        createTable($dbname, $tablename, $username, $password, $hostname);
+    }else{
+        echo "Please provide the correct database user and host information.";
     }
 }
 //Create the users tabel if the Db connection is successful
@@ -111,7 +111,6 @@ function createTable($dbname, $tablename, $username, $password, $hostname){
         die("Connection failed: " . mysqli_connect_error());
         return false;
     }else{
-   
         $sql = "CREATE TABLE " . $tablename . " (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             firstname VARCHAR(30) NOT NULL,
@@ -136,7 +135,6 @@ function createTable($dbname, $tablename, $username, $password, $hostname){
                     $sql_drop_table = "DROP TABLE IF EXISTS " . $tablename;
                     if (mysqli_query($dbConnection, $sql_drop_table)) {
                         //success
-                        echo "The table has been rebuild. ";
                         if (mysqli_query($dbConnection, $sql)) {
                             echo "Table " .$tablename . " created successfully.";
                         } else {
@@ -144,7 +142,7 @@ function createTable($dbname, $tablename, $username, $password, $hostname){
                             exit(1);
                         }
                     }else{
-                        echo "Something went wrong, could not drop table. ";
+                        echo "Something went wrong, could not drop the table. ";
                     }
                 }
             }
@@ -152,12 +150,16 @@ function createTable($dbname, $tablename, $username, $password, $hostname){
     mysqli_close($dbConnection);
 }
 //Insert the user records into the data-base
-function updateUsers($userRecords, $dbname, $username, $password, $hostname, $tablename){
+function updateUsers($userRecords, $dbname, $username, $password, $hostname, $tablename, $issetdryrun){
     //If the email is already in the system the record will be rejected - email unique value
     $duplicateRedordFound = false;
     $successRecordCount = 0;
     $successMessage = "record has";
     $dbConnection = mysqli_connect($hostname, $username, $password, $dbname);
+    if($issetdryrun == 1){
+        //Setting up for a test run only
+        mysqli_autocommit($dbConnection,FALSE);
+    }
     if (!$dbConnection) {
         die("Connection failed: " . mysqli_connect_error());
         return false;
@@ -190,25 +192,29 @@ function updateUsers($userRecords, $dbname, $username, $password, $hostname, $ta
             }
         }
     }
-    //Print out the list of duplicate records
+    //Print out the list of records addeed to the database and duplicate records not added.
     if($successRecordCount > 1 )$successMessage = "records have";
     if($successRecordCount > 0)echo "\n" . $successRecordCount . " " . $successMessage . " been added to the database";
     if($duplicateRedordFound)echo $duplicateRecords;
+    if($issetdryrun == 1){
+        // Rollback transaction
+        mysqli_rollback($dbConnection);
+        echo "Dry run was set: This was a successful test run and no data has been added to the database.";
+    }
     mysqli_close($dbConnection);
 }
 //Validate the db user details input
 function validateUserDetails($username, $password, $hostname){
     $validated = true;
     if(empty($username) || empty($password) || empty($hostname)){
-        echo "Please provide the correct database user and host information.";
         $validated = false;
     }
     return $validated;
 }
-//read in the csv file and process the data.
-/* The csv format must have header files which will be used to count the columns
+/* Read in the csv file and process the data.
+ * The csv format must have header files which will be used to count the columns
  * and are further ignored when processing the user data. 
- * The deliminator must be a coma, ",".
+ * The deliminator must be a comma, ",".
  */
 function getData($filename){
     if (file_exists($filename) && ($scvData = file($filename))!==false ) {
@@ -223,7 +229,6 @@ function getData($filename){
             $email = addslashes(trim(strtolower($scvData[$a][2])));
             $email = addslashes(filter_var($email, FILTER_SANITIZE_EMAIL));
             if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                //$dbrecords = $dbrecords . "('" . $firstname . "', '" . $lastname . "', '" . $email . "'),";
                 $record = "('" . $firstname . "', '" . $lastname . "', '" . $email . "')";
                 array_push($dbrecordsarray, $record);
             } else {
@@ -234,11 +239,7 @@ function getData($filename){
         echo "Please provide a valid file name and path. \nThe file could not be found or could not open the file.";
     }
     fclose($scvData);
-    //echo "\nEnd of getData.\n\n";
-    //echo "Records: " . $dbrecordsarray[1];
     echo "\n\nFollowing records were rejeted - invalid emal format:\n" . $dbrejets;
-    //Insert the valid user records into the database
-    //$dbrecords = substr($dbrecords, 0, -1);
     return $dbrecordsarray;
 }
 //initial process of the user data to put all data into arrays.
